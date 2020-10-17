86bd508e16d6a9614414782e7b8aafafc56df6c5
myin <minyin142p@gmail.com>
2020-10-17 19:46:42 +0200

    Add anim_combine script

---
+++ README.md
@@ -0,0 +1,4 @@
+### Scripts
+
+- **anim_combine.py**: combines multiple sprites with incrementing ending number to one image (horizontal)
+  - E.g anim0.png, anim1.png -> combined to anim.png
---
+++ anim_combine.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python3
+
+import os
+import re
+import sys
+from PIL import Image
+
+if len(sys.argv) < 3:
+    print('Missing arguments')
+    exit()
+
+DIR = sys.argv[1]
+OUTPUT = sys.argv[2]
+GROUPS = {}
+
+if not os.path.exists(DIR):
+    print('Directory '+DIR+' does not exist')
+    exit()
+
+if not os.path.exists(OUTPUT):
+    os.makedirs(OUTPUT)
+
+for entry in os.scandir(DIR):
+    pattern = re.compile(r"\d+\.png$")
+    prefix = pattern.split(entry.name)[0]
+    if GROUPS.get(prefix) is None:
+        GROUPS[prefix] = []
+
+    GROUPS[prefix].append(entry.path)
+
+for k in GROUPS:
+    images = [Image.open(x) for x in GROUPS[k]]
+    widths, heights = zip(*(i.size for i in images))
+
+    total_width = sum(widths)
+    max_height = max(heights)
+
+    new_im = Image.new('RGBA', (total_width, max_height))
+
+    x_offset = 0
+    for im in images:
+        new_im.paste(im, (x_offset, 0))
+        x_offset += im.size[0]
+
+    file_name = k
+    if not file_name.endswith('.png'):
+        file_name += '.png'
+
+    new_im.save(OUTPUT + '/' + file_name)
