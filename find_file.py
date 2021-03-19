019250267cc165f242aa10f926468549cad8803a
Min Yin <min.yin@ucs.at>
2021-03-19 13:51:36 +0100

    find_file: format recursive output

--- find_file.py
+++ find_file.py
@@ -21,12 +21,14 @@ files = {}
 
 def search_in_folder(dir, depth=1):
     print(f'Search in directory {dir}')
+    files[dir] = {}
+
     for filename in os.listdir(dir):
         path = os.path.join(dir, filename)
         if os.path.isfile(path):
             if args.match_file_name:
                 if txt in filename:
-                    files[path] = ''
+                    files[dir][filename] = ''
             else:
                 with open(path, 'r') as f:
                     try:
@@ -36,7 +38,7 @@ def search_in_folder(dir, depth=1):
                             match_index = content.index(txt)
                             start = match_index - snippet_size
                             end = match_index + len(txt) + snippet_size
-                            files[path] = content[start:end].replace(
+                            files[dir][filename] = content[start:end].replace(
                                 txt, color(txt, Colors.FAIL))
                     except:
                         print(f'Failed to read content of file {path}')
@@ -47,8 +49,12 @@ def search_in_folder(dir, depth=1):
 print(f'Searching for text "{txt}"')
 search_in_folder(FOLDER)
 
-for f in files:
-    print(color(f + ':', Colors.HEADER))
-    print(files[f])
+for dir in files:
+    for f in files[dir]:
+        path = f
+        if max_depth > 1:
+            path = os.path.join(dir, f)
+        print(color(path + ':', Colors.HEADER))
+        print(files[dir][f])
 
 print(color(f'\nFound in {len(files)} files', Colors.OKGREEN))
