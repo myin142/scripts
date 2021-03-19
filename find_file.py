302714d007513641820f2fab210dc533dada1be5
Min Yin <min.yin@ucs.at>
2021-03-19 12:03:04 +0100

    find_file: match file name + snippet size arg

--- find_file.py
+++ find_file.py
@@ -1,30 +1,38 @@
 import os
 import sys
+from argparse import ArgumentParser
 from shared import Colors, color
 
-# Arg 1: folder to search
-# Arg 2: text to search
+parser = ArgumentParser()
+parser.add_argument('folder')
+parser.add_argument('text')
+parser.add_argument('--match-file-name', default=False, action='store_true')
+parser.add_argument('--snippet-size', default=40, type=int)
+parser.add_argument('--recursive', default=False, action='store_true') # TODO
+args = parser.parse_args()
 
-if len(sys.argv) < 3:
-    print('Missing arguments')
-    exit()
 
-FOLDER = sys.argv[1]
-txt = sys.argv[2]
-SNIPPET_SIZE = 40
+FOLDER = args.folder
+txt = args.text
+snippet_size = args.snippet_size
 
 files = {}
 for filename in os.listdir(FOLDER):
     path = os.path.join(FOLDER, filename)
     if os.path.isfile(path):
-        with open(path, 'r') as f:
-            content = f.read()
-            if txt in content:
-                # TODO: display multiple matches in a single file
-                match_index = content.index(txt)
-                start = match_index - SNIPPET_SIZE
-                end = match_index + len(txt) + SNIPPET_SIZE
-                files[filename] = content[start:end].replace(txt, color(txt, Colors.FAIL))
+        if args.match_file_name:
+            if txt in filename:
+                files[filename] = ''
+        else:
+            with open(path, 'r') as f:
+                content = f.read()
+                if txt in content:
+                    # TODO: display multiple matches in a single file
+                    match_index = content.index(txt)
+                    start = match_index - snippet_size
+                    end = match_index + len(txt) + snippet_size
+                    files[filename] = content[start:end].replace(
+                        txt, color(txt, Colors.FAIL))
 
 print('Searched folder {} for text "{}"'.format(FOLDER, txt))
 for f in files:
