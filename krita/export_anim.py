7f11605379dae052024642064e45339ad3bc71fe
myin <minyin142p@gmail.com>
2022-11-29 18:22:41 +0100

    clean export animation script

--- krita/export_anim.py
+++ krita/export_anim.py
@@ -2,20 +2,15 @@ from PyQt5.QtWidgets import *
 from PyQt5.QtCore import *
 from krita import InfoObject
 
-folder = QFileDialog.getExistingDirectory()
-print(folder)
-
 doc = Krita.instance().activeDocument()
+sel = doc.selection()
 node = doc.activeNode()
 
-print(node)
-
-sel = doc.selection()
-if sel:
+if sel and node:
+    folder = QFileDialog.getExistingDirectory()
     rect = QRect(sel.x(), sel.y(), sel.width(), sel.height())
 
     info = InfoObject()
-    
     for i in range(0, doc.animationLength()):
         file = '{}/{}_{}.png'.format(folder, node.name(), i)
         doc.setCurrentTime(i)
