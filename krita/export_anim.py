d9e975382aeda698555ae75b5d42476dec0cc025
myin <minyin142p@gmail.com>
2022-11-29 18:33:26 +0100

    export animation as gif

--- krita/export_anim.py
+++ krita/export_anim.py
@@ -1,6 +1,7 @@
 from PyQt5.QtWidgets import *
 from PyQt5.QtCore import *
 from krita import InfoObject
+import imageio
 
 doc = Krita.instance().activeDocument()
 sel = doc.selection()
@@ -11,7 +12,12 @@ if sel and node:
     rect = QRect(sel.x(), sel.y(), sel.width(), sel.height())
 
     info = InfoObject()
+    fps = doc.framesPerSecond()
+    images = []
     for i in range(0, doc.animationLength()):
         file = '{}/{}_{}.png'.format(folder, node.name(), i)
         doc.setCurrentTime(i)
         node.save(file, doc.xRes(), doc.yRes(), info, rect)
+        images.append(imageio.imread(file))
+    final_file = '{}/{}.gif'.format(folder, node.name())
+    imageio.mimsave(final_file, images, 'GIF', fps=fps)
