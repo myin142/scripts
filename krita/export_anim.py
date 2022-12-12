dca27718dd17dc0ea268ac7088fb8b016b2b7230
myin <minyin142p@gmail.com>
2022-12-12 20:46:49 +0100

    krita: check if folder was selected

--- krita/export_anim.py
+++ krita/export_anim.py
@@ -9,15 +9,16 @@ node = doc.activeNode()
 
 if sel and node:
     folder = QFileDialog.getExistingDirectory()
-    rect = QRect(sel.x(), sel.y(), sel.width(), sel.height())
+    if folder:
+        rect = QRect(sel.x(), sel.y(), sel.width(), sel.height())
 
-    info = InfoObject()
-    fps = doc.framesPerSecond()
-    images = []
-    for i in range(0, doc.animationLength()):
-        file = '{}/{}_{}.png'.format(folder, node.name(), i)
-        doc.setCurrentTime(i)
-        node.save(file, doc.xRes(), doc.yRes(), info, rect)
-        images.append(imageio.imread(file))
-    final_file = '{}/{}.gif'.format(folder, node.name())
-    imageio.mimsave(final_file, images, 'GIF', fps=fps)
+        info = InfoObject()
+        fps = doc.framesPerSecond()
+        images = []
+        for i in range(0, doc.animationLength()):
+            file = '{}/{}_{}.png'.format(folder, node.name(), i)
+            doc.setCurrentTime(i)
+            node.save(file, doc.xRes(), doc.yRes(), info, rect)
+            images.append(imageio.imread(file))
+        final_file = '{}/{}.gif'.format(folder, node.name())
+        imageio.mimsave(final_file, images, 'GIF', fps=fps)
--- krita/export_layers.py
+++ krita/export_layers.py
@@ -8,15 +8,17 @@ node = doc.activeNode()
 
 if node and node.childNodes():
     folder = QFileDialog.getExistingDirectory()
-    name = node.name()
-    info = InfoObject()
-    children = node.childNodes()
     
-    rect = QRect(sel.x(), sel.y(), sel.width(), sel.height()) if sel else doc.bounds()
-    for i in range(0, len(children)):
-        print('Exporting Layer {}'.format(i))
-        child = children[i]
-        file = '{}/{}_{}.png'.format(folder, name, i)
-        child.save(file, doc.xRes(), doc.yRes(), info, rect)
+    if folder:
+        name = node.name()
+        info = InfoObject()
+        children = node.childNodes()
+
+        rect = QRect(sel.x(), sel.y(), sel.width(), sel.height()) if sel else doc.bounds()
+        for i in range(0, len(children)):
+            child = children[i]
+            file = '{}/{}_{}.png'.format(folder, name, i)
+            print('Exporting Layer {} to {}'.format(i, file))
+            child.save(file, doc.xRes(), doc.yRes(), info, rect)
 
         
