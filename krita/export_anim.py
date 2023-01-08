0cb3d00932db20d2af4327e823912f5fd5abca0d
myin <minyin142p@gmail.com>
2023-01-08 19:30:52 +0100

    export recursive animation

--- krita/export_anim.py
+++ krita/export_anim.py
@@ -7,18 +7,40 @@ doc = Krita.instance().activeDocument()
 sel = doc.selection()
 node = doc.activeNode()
 
+def export(node, i):
+    if node.name().startswith("Group"):
+        for child in node.childNodes():
+            export(child, i)
+    else:
+        if not node.animated() and i != 0:
+            return
+       
+        if node.animated() and not node.hasKeyframeAtTime(i):
+            return
+                    
+        info = InfoObject()
+        rect = QRect(sel.x(), sel.y(), sel.width(), sel.height())
+        name = node.name()
+        file = '{}/{}_{}.png'.format(folder, name, i)
+        
+        node.save(file, doc.xRes(), doc.yRes(), info, rect)
+        print("Export layer {} at frame {}".format(name, i))
+
 if sel and node:
     folder = QFileDialog.getExistingDirectory()
     if folder:
-        rect = QRect(sel.x(), sel.y(), sel.width(), sel.height())
-
-        info = InfoObject()
         fps = doc.framesPerSecond()
-        images = []
-        for i in range(0, doc.animationLength()):
-            file = '{}/{}_{}.png'.format(folder, node.name(), i)
+        #images = []
+
+        name = node.name()
+        
+        length = doc.animationLength()
+        print("Exporting {} frames".format(length))
+        for i in range(0, length):
+            print("Exporting frame {}".format(i))
             doc.setCurrentTime(i)
-            node.save(file, doc.xRes(), doc.yRes(), info, rect)
-            images.append(imageio.imread(file))
-        final_file = '{}/{}.gif'.format(folder, node.name())
-        imageio.mimsave(final_file, images, 'GIF', fps=fps)
+            export(node, i)
+            
+            #images.append(imageio.imread(file))
+        #final_file = '{}/{}.gif'.format(folder, node.name())
+        #imageio.mimsave(final_file, images, 'GIF', fps=fps)
